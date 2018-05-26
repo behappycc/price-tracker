25a301d29ecf8ace7dc4811cc40ffb7bb801ee6a
Hubert <behappyccc@gmail.com>
2018-05-26 10:33:30 +0800

    Add books_spider v1

--- books_spider/books_spider/pipelines.py
+++ books_spider/books_spider/pipelines.py
@@ -4,19 +4,23 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
-
+import sqlite3
+# DROP TABLE books
 class BooksSpiderPipeline(object):
     def open_spider(self, spider):
-        self.conn = sqlite3.connect ("book.sqlite") 
+        self.conn = sqlite3.connect ("books.sqlite") 
         self.cur = self.conn.cursor() 
-        self.cur.execute("create tab;e of not exists books title varchar(100), content text, time varchar(50)")
+        self.cur.execute("CREATE TABLE if not exists books(title TEXT, author TEXT, price INTEGER)")
 
     def close_spider(self, spider):
         self.conn.commit()
         self.conn.close()
 
     def process_item(self, item, spider):
-        sql='insert'
-        self.cur.execute(sql)
+        title = item['title']
+        author = ', '.join(item['author'])
+        price = item['price']
+        self.cur.execute('''INSERT INTO books(title, author, price) VALUES(?,?,?)''', (title, author, price))
         return item
+
+
